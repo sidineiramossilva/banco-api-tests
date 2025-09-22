bead2fb623ae37b40cd082a799abd84aa714f6c2
Sidinei <sydney.ramos@hotmail.com>
2025-09-22 20:19:38 -0300

    Adicionando a biblioteca .env para variáveis globais

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -1,11 +1,12 @@
 const request = require('supertest');
 const { expect } = require('chai');
+require('dotenv').config()
 
 describe('Transferências', () => {
     describe('POST /transferencias', () => {
         it('Deve retornar sucesso com 201 quando o valor da transferência for igual ou acima de R$ 10,00', async () => {
             // Capturar o token
-            const respostaLogin = await request('http://localhost:3000')
+            const respostaLogin = await request(process.env.BASE_URL)
                 .post('/login')
                 .set('Content-Type', 'application/json')
                 .send({
@@ -15,7 +16,7 @@ describe('Transferências', () => {
 
             const token = respostaLogin.body.token
 
-            const resposta = await request('http://localhost:3000')
+            const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Bearer ${token}`)
