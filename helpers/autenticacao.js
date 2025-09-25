f7b36b84b7a159983e595350d267f9be1d5affb4
Sidinei <sydney.ramos@hotmail.com>
2025-09-25 10:32:32 -0300

    adicionando testes POST no login

--- helpers/autenticacao.js
+++ helpers/autenticacao.js
@@ -3,6 +3,8 @@ const postLogin = require('../fixtures/postLogin.json')
 
 const obterToken = async (usuario, senha) => {
     const bodyLogin = { ...postLogin}
+    bodyLogin.username = usuario
+    bodyLogin.senha = senha
     const respostaLogin = await request(process.env.BASE_URL)
         .post('/login')
         .set('Content-Type', 'application/json')
